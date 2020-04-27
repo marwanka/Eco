7cf37d55a7c44749a5009eab0fe28856359539d9
Ivan Cherkashin <ogurka8772@gmail.com>
2020-04-27 10:26:23 +0700

    Added authorization

--- index.js
+++ index.js
@@ -1,10 +1,90 @@
 const config = require('./config.json');
 const fs = require('fs');
+const sha512 = require('js-sha512');
 
+
+//Influx database
 const Influx = require('influxdb-nodejs');
 const influx = new Influx(config.influx.uri);
-
 influx.schema('eco_dat', { city: 's', pressure: 'f', temperature: 'f', humidity: 'f'}, {}, { stripUnknown: true });
+
+
+//MongoDB database
+const mongoose = require('mongoose');
+mongoose.connect(config.mongodb.uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Redis database
+const redis = require("redis");
+const redisClient = redis.createClient(config.redis.uri);
+
+
+//-----------------------------------
+//     Passport.js Authorization
+//-----------------------------------
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const expressSession = require('express-session');
+const User = require('./user');
+
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use('login', new LocalStrategy({
+    passReqToCallback : true
+  },
+  (req, username, password, done) => {
+    User.findOne({ 'username' :  username },
+      (err, user) => {
+        if (err)
+          return done(err);
+        if (!user){
+          return done(null, false,
+                req.flash('message', 'User Not found.'));
+        }
+        if (!(user.password === sha512(password))){
+          return done(null, false,
+              req.flash('message', 'Invalid Password'));
+        }
+        return done(null, user);
+      }
+    );
+}));
+
+passport.use('signup', new LocalStrategy({
+    passReqToCallback : true
+  },
+  (req, username, password, done) => {
+    User.findOne({'username':username},function(err, user) {
+      if (err){
+        return done(err);
+      }
+      if (user) {
+        return done(null, false,
+           req.flash('message','User Already Exists'));
+      } else {
+        var newUser = new User();
+        // set the user's local credentials
+        newUser.username = username;
+        newUser.password = sha512(password);
+
+        newUser.save((err) => {
+          if (err){
+            console.error(err);
+            throw err;
+          }
+          return done(null, newUser);
+        });
+      }
+    });
+  })
+);
 
 //--------------------------
 //     Base express app
@@ -12,16 +92,78 @@ influx.schema('eco_dat', { city: 's', pressure: 'f', temperature: 'f', humidity:
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const RedisStore = require('connect-redis')(require('express-session'));
+const cookieParser = require('cookie-parser');
 
+const sessions = require('express-session')({
+  store: new RedisStore({client: redisClient}),
+  secret: "super_secret_key",
+  resave: true,
+  rolling: true,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 10 * 60 * 1000,
+    httpOnly: false,
+  },
+});
+
+const flash = require('express-flash')
+app.use(flash())
 app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(cookieParser());
+app.use(sessions);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-})
+const noAuthenticated = (req, res, next) => {
+  if (!req.isAuthenticated())
+    return next();
+  res.redirect('/');
+};
+
+app.get('/signup', noAuthenticated, (req, res) => {
+  res.render('signup', { message: req.flash('message') });
+});
+
+app.get('/login', noAuthenticated, (req, res) => {
+  res.render('login', { message: req.flash('message') });
+});
+
+app.post('/login', passport.authenticate('login', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash : true
+}));
+
+app.post('/signup', passport.authenticate('signup', {
+  successRedirect: '/',
+  failureRedirect: '/signup',
+  failureFlash : true
+}));
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+const isAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('/login');
+};
+
+app.get('/', isAuthenticated, (req, res) => {
+  console.log(req.user);
+  if(!req.user) { res.redirect('/login'); }
+  res.sendFile(__dirname + '/public/client.html');
+});
+
+
 
 //----------------------------
 //     Base socket server
@@ -74,14 +216,10 @@ const collectData = () => {
   });
 };
 
-collectData();
-const collectWeatherCycle = () => {
-  setTimeout(()=> {
-    collectData();
-    collectWeatherCycle();
-  }, 1800000);
-};
-collectWeatherCycle();
+setInterval(()=> {
+  collectData();
+  collectWeatherCycle();
+}, 1800000)
 
 http.listen(80, ()=>{
   console.log("Server successfully started on port 80");
