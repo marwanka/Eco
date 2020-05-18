692dceb9e51f778e228354375a4d757cb855a160
Ivan Cherkashin <ogurka8772@gmail.com>
2020-05-18 11:27:10 +0700

    Fixed client

---
+++ public/precache-manifest.8b45671770ab916158919e21e307003a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613c3beaf339d2144bf7ae0396722ad6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d75bdb894624459bf416",
+    "url": "/static/css/2.fb203e32.chunk.css"
+  },
+  {
+    "revision": "ff6b2b88f4543b1e5cfa",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d75bdb894624459bf416",
+    "url": "/static/js/2.1fbc8736.chunk.js"
+  },
+  {
+    "revision": "3ffd37162a88e101799dd00390ea59c9",
+    "url": "/static/js/2.1fbc8736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff6b2b88f4543b1e5cfa",
+    "url": "/static/js/main.708904ce.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
